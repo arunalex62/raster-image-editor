9b8c52c7ce06a8464c7a6cfc70cdfda7ad6a8472
Arun Alex <arunalex62@gmail.com>
2024-08-06 23:37:26 -0700

    Added a menu bar, status bar, and file open dialog + image viewing functionality.

--- app/mainWindow.cpp
+++ app/mainWindow.cpp
@@ -1,27 +1,52 @@
 #include "mainWindow.hpp"
+#include "menuBar.hpp"
+#include "statusBar.hpp"
+#include "qwidget.h"
+#include "qlabel.h"
+#include <qimage.h>
+#include <qpixmap.h>
+#include <iostream>
+#include <qfiledialog.h>
 
 MainWindow::MainWindow( QWidget *parent, const char *name )
-        : QMainWindow( parent, name )
+        : QMainWindow( parent, name ), imageWidth{0}, imageHeight{0},
+          fileName{""}, imageDisplay{new QLabel(this)}
 {
-    QPopupMenu *fileMenu = new QPopupMenu( this );
-    menuBar()->insertItem("File", fileMenu);
-    fileMenu->insertItem( "Open", this, SLOT(open()));
-    fileMenu->insertItem( "Export", this, SLOT(open()));
-
-    QPopupMenu *editMenu = new QPopupMenu( this );
-    menuBar()->insertItem("Edit", editMenu);
-    editMenu->insertItem( "Resize Canvas", this, SLOT(open()));
-
-    QPopupMenu *viewMenu = new QPopupMenu( this );
-    menuBar()->insertItem("View", viewMenu);
-    viewMenu->insertItem( "Grid-Lines Toggle", this, SLOT(open()));
-
-    QPopupMenu *helpMenu = new QPopupMenu( this );
-    menuBar()->insertSeparator();
-    menuBar()->insertItem("Help", helpMenu);
-    helpMenu->insertItem( "About", this, SLOT(open()));
+    MenuBar::setupMenuBar(this);
+    imageDisplay->setAlignment(Qt::AlignCenter);
+    // if(imagePixmap.load(fileName)) {
+    //     std::cout << "Works!\n";
+    //     imageDisplay->setPixmap(imagePixmap);
+    //     imageDisplay->show();
+    //     setCentralWidget(imageDisplay);
+    // }
+    StatusBar::setStatusBar(this);
 }
 
 void MainWindow::open() {
     QMessageBox::about(this, "Test", "This is a test app with a menu bar.");
+}
+
+void MainWindow::fileOpen() {
+    // Taken from QFileDialog Qt Class Reference Page:
+    // https://doc.qt.io/archives/3.3/qfiledialog.html
+    fileName = QFileDialog::getOpenFileName(
+                    "/home",
+                    "Images (*.png *.jpg)",
+                    this,
+                    "open file dialog",
+                    "Choose a file" );
+    // If filename is found, load the image and display it.
+    if (fileName != "") {
+        if(imagePixmap.load(fileName)) {
+            imageDisplay->setPixmap(imagePixmap);
+            imageDisplay->show();
+            setCentralWidget(imageDisplay);
+            imageWidth = imagePixmap.width();
+            imageHeight = imagePixmap.height();
+            // Update status bar with width/height of new image.
+            StatusBar::setStatusBar(this);
+        }
+    }
+       
 }
