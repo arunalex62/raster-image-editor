9d3fd31bda7a3470a29cd75db18239042d623ac1
Arun Alex <arunalex62@gmail.com>
2024-08-10 23:49:23 -0700

    Added comments to statusBar.hpp describing documentation of methods.

--- include/statusBar.hpp
+++ include/statusBar.hpp
@@ -3,8 +3,20 @@
 
 #include "mainWindow.hpp"
 
+// Class that implements and defines methods for
+// initializing and dynamically updating the
+// status bar of the main window with
+// image-related information.
+
 class StatusBar {
 public:
+    // Method for initializing the sub-sections of the 
+    // status bar, such as the image dimensions section
+    // and the mouse position section.
+    static void setupStatusBar(MainWindow *mainWindow);
+    // Method for updating the status bar with information
+    // during the execution of the program.
+    // This includes mouse position and image dimensions.
     static void setStatusBar(MainWindow *mainWindow);
 };
 
