9e5a989f280b413c906c614abf7d58649e5fd7a2
Arun Alex <arunalex62@gmail.com>
2024-08-12 00:03:21 -0700

    Added comments to menubar files to describe functionality.

--- app/menuBar.cpp
+++ app/menuBar.cpp
@@ -5,28 +5,35 @@
 // Creating/displaying menu bar items with their click functions linked.
 void MenuBar::setupMenuBar(QMainWindow *mainWindow)
 {
+    // Initial variable initialization to link to existing
+    // menu bar on the main window.
     QMenuBar *menuBar = mainWindow->menuBar();
+
+    // Adding file dropdown with related sub-options.
     QPopupMenu *fileMenu = new QPopupMenu( mainWindow );
     menuBar->insertItem("File", fileMenu);
     fileMenu->insertItem( "New", mainWindow, SLOT(fileNew()));
     fileMenu->insertItem( "Open", mainWindow, SLOT(fileOpen()));
     fileMenu->insertItem( "Export", mainWindow, SLOT(fileExport()));
 
+    // Adding edit dropdown with related sub-options.
     QPopupMenu *editMenu = new QPopupMenu( mainWindow );
     menuBar->insertItem("Edit", editMenu);
     editMenu->insertItem( "Resize Canvas", mainWindow, SLOT(editResizeCanvas()));
 
+    // Adding view dropdown with related sub-options.
     QPopupMenu *viewMenu = new QPopupMenu( mainWindow );
     menuBar->insertItem("View", viewMenu);
     viewMenu->insertItem( "Grid-Lines Toggle", mainWindow, SLOT(viewGridlinesToggle()));
     viewMenu->insertItem( "Reset Camera", mainWindow, SLOT(viewResetCamera()));
 
-
+    // Adding brush dropdown with related sub-options.
     QPopupMenu *brushMenu = new QPopupMenu( mainWindow );
     menuBar->insertItem("Brush", brushMenu);
     brushMenu->insertItem ( "Select Color", mainWindow, SLOT(colorDialog()));
     brushMenu->insertItem ( "Set Brush Size", mainWindow, SLOT(brushSizeDialog()));
 
+    // Adding tools dropdown with related sub-options.
     QPopupMenu *toolsMenu = new QPopupMenu( mainWindow );
     menuBar->insertItem("Tools", toolsMenu);
     toolsMenu->insertItem ( "Brush", mainWindow, SLOT(toolsBrush()));
@@ -34,6 +41,7 @@ void MenuBar::setupMenuBar(QMainWindow *mainWindow)
     toolsMenu->insertItem ( "Fill", mainWindow, SLOT(toolsFill()));
     toolsMenu->insertItem ( "Colour Picker", mainWindow, SLOT(toolsColourPicker()));
 
+    // Adding help dropdown with related sub-options.
     QPopupMenu *helpMenu = new QPopupMenu( mainWindow );
     menuBar->insertSeparator();
     menuBar->insertItem("Help", helpMenu);
--- include/menuBar.hpp
+++ include/menuBar.hpp
@@ -3,6 +3,10 @@
 
 #include <qmainwindow.h>
 
+// Single class method to setup the menu bar with
+// all of the buttons/linked methods.
+
+// This is initialized when the program is initialized.
 class MenuBar {
 public:
     static void setupMenuBar(QMainWindow *mainWindow);
