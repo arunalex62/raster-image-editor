3001c1e80352f005b1c0dad6d3b0950a6f63941d
Arun Alex <arunalex62@gmail.com>
2024-08-11 20:45:04 -0700

    Added comments to brushSizeDialogWidget to describe functionality.

--- app/brushSizeDialogWidget.cpp
+++ app/brushSizeDialogWidget.cpp
@@ -6,18 +6,26 @@
 brushSizeDialogWidget::brushSizeDialogWidget( MainWindow *parent, const char *name )
         : QDialog( parent, name )
 {
+    // Get the current brush size to use as the initial slider value.
     const int brush_size = parent->imageView->penWidth();
     QVBoxLayout *layout = new QVBoxLayout(this);
+
+    // displayString is the text the user sees while using the slider.
     displayString = new QLabel(" Set Brush Size: " + std::to_string(brush_size), this);
 
     layout->addWidget(displayString);
-
+    // Slider created with range [1, 30].
+    // Has initial value of current brush size.
     QSlider * slider = new QSlider( Horizontal, this, "slider" );
     slider->setRange(1, 30);
     slider->setValue (brush_size);
     layout->addWidget(slider);
 
+    // When the slider value is changed, it updates the displayString
+    // in real-time to the user to show the selected size.
     connect(slider, SIGNAL(valueChanged(int)), this, SLOT(updateSize(int)));
+    // The same happens when the slider value changes, setting the
+    // actual brush size of the application in real-time.
     connect(slider, SIGNAL(valueChanged(int)), parent->imageView, SLOT(setPenWidth(int)));
 }
 
--- include/brushSizeDialogWidget.hpp
+++ include/brushSizeDialogWidget.hpp
@@ -2,12 +2,18 @@
 #include <qdialog.h>
 #include <qlabel.h>
 
+// Simple class to show a brush size slider widget
+// to the user to select their brush size.
 class brushSizeDialogWidget : public QDialog
 {
+    // Allows for a tight integration with signal/slot mechanism
+    // between different parts of the application.
     Q_OBJECT
 public:
     brushSizeDialogWidget( MainWindow *parent=0, const char *name=0 );
 private slots:
+    // Helper function to update the string that appears to the user
+    // when selecting their brush size.
     void updateSize(int value);
 private:
     QLabel *displayString;
